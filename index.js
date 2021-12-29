2527609c4d723de174f3e2c6110e0fcb64990c6c
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-12-29 10:50:08 +0300

    done

--- index.js
+++ index.js
@@ -32,10 +32,10 @@ function weatherAPI(units = temperatureUnits) {
     let coordinatePromise = city.value === '' ? getUserLocation() : getCityGeolocation(city.value);
      
     coordinatePromise
-        .then(([
-                lat,
-                lng
-        ]) =>
+        .then(({
+            lat,
+            lng
+        }) =>
             fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherApiKey}&units=${units}&lang=${curLang}`)
         )
         .then((resp) => resp.json())
