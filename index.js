895bdcdd741ef6bdd6cabde86faf2c80785f8b4b
GuzeevZhenya <shyst.evgeny@mail.ru>
2022-01-18 12:12:44 +0300

    fix bugs

--- index.js
+++ index.js
@@ -1,112 +1,26 @@
 import { languagesText } from './weatherLanguage.js';
 
-
 const searchButton = document.querySelector('.input_submit');
 const city = document.querySelector('.input_search');
 const weatherApiKey = 'ea04db02d64d4b2b6453bfc814cd3cf9';
 const geolocationApiKey = 'hqZM0yzr5AMhh6Au5FZzvResHAEELg2N';
-const opencagedataKey = '236efb487f0e461d9f1e4483d233acac';
 const refreshBtn = document.querySelector('.refresh');
 let map;
-
 let lang = document.querySelector('.lang');
-
 const timeInfo = document.querySelector('.current_time');
 let temperatureBlock = document.querySelector('.temperature');
-let index = 0;
+
 
 let langLocalStorage = localStorage.getItem('lang');
 let curLang = langLocalStorage;
-localStorage.setItem('lang', curLang);
 
+localStorage.setItem('lang', curLang);
 let temperatureLocalStorage = localStorage.getItem('temperature');
+
 let temperatureUnits = temperatureLocalStorage;
 localStorage.setItem('temperature', temperatureUnits);
 
-function weatherAPI(units = temperatureUnits) {
-  let coordinatePromise = city.value === '' ? getUserLocation() : getCityGeolocation(city.value);
-  coordinatePromise
-    .then((data) => {
-      const { lat, lng } = data;
-      return fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherApiKey}&units=${units}&lang=${curLang}`,
-      );
-    })
-    .then((resp) => resp.json())
-    .then((data) => createWeatherBlocks(data, curLang));
-  // .catch((e) => alert(e));
-}
-
-function updateUserLocation(units = temperatureUnits) {
-  getUserLocation()
-    .then(({ lat, lng }) =>
-      fetch(
-        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherApiKey}&units=${units}&lang=${curLang}`,
-      ),
-    )
-    .then((resp) => resp.json())
-    .then((data) => createWeatherBlocks(data, curLang));
-  // .catch((e) => alert(e));
-}
-
-function getCityGeolocation(cityName) {
-  return fetch(
-    `https://open.mapquestapi.com/geocoding/v1/address?key=${geolocationApiKey}&location=${cityName}`,
-  )
-    .then((resp) => resp.json())
-    .then((data) => data.results[0].locations[0].latLng);
-}
-
-function getUserLocation() {
-  return fetch('https://ipinfo.io/json?token=2a0ee799551687')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      const [ lat, lng ] = data.loc.split(',');
-      return { lat, lng };
-    })
-    .catch((err) => {
-      alert('Something went wrong');
-      console.log('err getUserLocation');
-    });
-}
-
-function createWeatherBlocks(dataInfo, curLang = 'en') {
-  createWeatherCard(dataInfo, curLang);
-  showWeatherDay(dataInfo);
-}
-
-function createWeatherCard(weatherInfo, curLang) {
-  const translateParams = {
-    currentCity: weatherInfo.city.name,
-    temperature: Math.floor(weatherInfo.list[0].main.temp_max),
-    feelsLike: Math.floor(weatherInfo.list[0].main.feels_like),
-    wind: weatherInfo.list[0].wind['speed'],
-    humidity: weatherInfo.list[0].main.humidity,
-    overcast: weatherInfo.list[0].weather[0]['description'],
-    latitude: weatherInfo.city.coord['lat'],
-    longitude: weatherInfo.city.coord['lon'],
-  };
-
-  document.querySelectorAll('[data-translate]').forEach((element) => {
-    const translateKey = element.dataset.translate;
-    element.textContent = languagesText[curLang][translateKey].replace(
-      `{{value}}`,
-      translateParams[translateKey],
-    );
-  });
-
-  city.placeholder = languagesText[curLang].searchCity;
-  searchButton.textContent = languagesText[curLang].btnSearch;
-  document.querySelector('.current_city').textContent = weatherInfo.city.name;
-  document.querySelector(
-    '.temperature_symbol',
-  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${weatherInfo.list[0].weather[0]
-    .icon}@2x.png">`;
-}
-
-function showWeatherDay(weatherInfo) {
+const showWeatherDay = (weatherInfo) => {
   let langForTime;
   let today = Date.now();
   let dayNow = new Date(today);
@@ -133,11 +47,93 @@ function showWeatherDay(weatherInfo) {
     ).innerHTML = `<img src="https://openweathermap.org/img/wn/${weatherInfo.list[`${i * 8}`]
       .weather[0].icon}@2x.png">`;
   }
-}
+};
+
+const createWeatherCard = (weatherInfo, curLang) => {
+  const translateParams = {
+    currentCity: weatherInfo.city.name,
+    temperature: Math.floor(weatherInfo.list[0].main.temp_max),
+    feelsLike: Math.floor(weatherInfo.list[0].main.feels_like),
+    wind: weatherInfo.list[0].wind['speed'],
+    humidity: weatherInfo.list[0].main.humidity,
+    overcast: weatherInfo.list[0].weather[0]['description'],
+    latitude: weatherInfo.city.coord['lat'],
+    longitude: weatherInfo.city.coord['lon'],
+  };
+
+  document.querySelectorAll('[data-translate]').forEach((element) => {
+    const translateKey = element.dataset.translate;
+    element.textContent = languagesText[curLang][translateKey].replace(
+      `{{value}}`,
+      translateParams[translateKey],
+    );
+  });
+
+  city.placeholder = languagesText[curLang].searchCity;
+  searchButton.textContent = languagesText[curLang].btnSearch;
+  document.querySelector('.current_city').textContent = weatherInfo.city.name;
+  document.querySelector(
+    '.temperature_symbol',
+  ).innerHTML = `<img src="https://openweathermap.org/img/wn/${weatherInfo.list[0].weather[0]
+    .icon}@2x.png">`;
+};
+
+const createWeatherBlocks = (dataInfo, curLang = 'en') => {
+  createWeatherCard(dataInfo, curLang);
+  showWeatherDay(dataInfo);
+};
+
+const getCityGeolocation = (cityName) => {
+  return fetch(
+    `https://open.mapquestapi.com/geocoding/v1/address?key=${geolocationApiKey}&location=${cityName}`,
+  )
+    .then((resp) => resp.json())
+    .then((data) => data.results[0].locations[0].latLng);
+};
+
+const getUserLocation = () => {
+  return fetch('https://ipinfo.io/json?token=2a0ee799551687')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const [ lat, lng ] = data.loc.split(',');
+      return { lat, lng };
+    });
+  // .catch((err) => {
+  //   alert('Something went wrong');
+  //   console.log('err getUserLocation');
+  // });
+};
+
+const weatherAPI = (units = temperatureUnits) => {
+  let coordinatePromise = city.value === '' ? getUserLocation() : getCityGeolocation(city.value);
+  coordinatePromise
+    .then((data) => {
+      const { lat, lng } = data;
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherApiKey}&units=${units}&lang=${curLang}`,
+      );
+    })
+    .then((resp) => resp.json())
+    .then((data) => createWeatherBlocks(data, curLang));
+};
+
+const updateUserLocation = (units = temperatureUnits) => {
+  getUserLocation()
+    .then(({ lat, lng }) =>
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${weatherApiKey}&units=${units}&lang=${curLang}`,
+      ),
+    )
+    .then((resp) => resp.json())
+    .then((data) => createWeatherBlocks(data, curLang));
+};
 
 const randomBackground = () => {
   const weatherBackground = document.querySelector('.weather');
   const backgrounds = [ 'url(img/1.jpg)', 'url(img/2.jpg)', 'url(img/3.jpg)' ];
+  let index = 0;
 
   let item = backgrounds[index];
   index++;
@@ -148,13 +144,13 @@ const randomBackground = () => {
   weatherBackground.style.backgroundSize = 'cover';
 };
 
-function toggleButtons(button) {
+const toggleButtons = (button) => {
   let buttonParent = button.parentElement;
   buttonParent.querySelectorAll('button').forEach((item) => {
     item.classList.remove('button_active');
   });
   button.classList.add('button_active');
-}
+};
 
 lang.addEventListener('click', (e) => {
   let target = e.target;
@@ -165,19 +161,12 @@ lang.addEventListener('click', (e) => {
   let language = localStorage.getItem('lang');
   curLang = language;
   weatherAPI();
-
-  // if (city.value == '') {
-  //     updateUserLocation();
-  // } else {
-  //     weatherAPI();
-  // }
 });
 
 temperatureBlock.addEventListener('click', (e) => {
   let target = e.target;
   toggleButtons(target);
   let temperatureName = target.getAttribute('data-info');
-  // temperatureUnits = temperatureName;
 
   localStorage.setItem('temperature', temperatureName);
   let temp = localStorage.getItem('temperature');
@@ -186,12 +175,12 @@ temperatureBlock.addEventListener('click', (e) => {
   weatherAPI();
 });
 
-function addZero(n) {
+const addZero = (n) => {
   //добавление 0 для времени
   return `0${n}`.slice(-2);
-}
+};
 
-function getTimeInfo() {
+const getTimeInfo = () => {
   let data = new Date();
   return {
     hour: data.getHours(),
@@ -200,30 +189,16 @@ function getTimeInfo() {
     dayName: data.getDay(),
     day: data.getDate(),
   };
-}
+};
 
-function showDateTime() {
+const showDateTime = () => {
   let dateTimeInfo = getTimeInfo();
   timeInfo.textContent = `${addZero(dateTimeInfo.hour)}:${addZero(dateTimeInfo.min)}:${addZero(
     dateTimeInfo.sec,
   )}`;
-}
+};
 
-function getUserMap() {
-  getUserLocation().then(({ lat, lng }) => init(lat, lng));
-}
-
-function getSearchMap() {
-  getCityGeolocation(city.value).then((data) => {
-    if (map) {
-      flyTo(data.lat, data.lng);
-    } else {
-      init(data.lat, data.lng);
-    }
-  });
-}
-
-function init(lat, lng) {
+const init = (lat, lng) => {
   mapboxgl.accessToken =
     'pk.eyJ1IjoiZmFuZ3VzIiwiYSI6ImNrcDN6cWUycTFmY2gycG13YXV4aGY0eHEifQ.a3Eu2Aj9YHQUeSlYJn2Xiw';
   map = new mapboxgl.Map({
@@ -232,9 +207,13 @@ function init(lat, lng) {
     center: [ lng, lat ],
     zoom: 10.2,
   });
-}
+};
 
-function flyTo(lat, lng) {
+const getUserMap = () => {
+  getUserLocation().then(({ lat, lng }) => init(lat, lng));
+};
+
+const flyTo = (lat, lng) => {
   map.flyTo({
     center: [ lng, lat ],
     essential: true,
@@ -242,7 +221,17 @@ function flyTo(lat, lng) {
     pitch: 45,
     bearing: 27,
   });
-}
+};
+
+const getSearchMap = () => {
+  getCityGeolocation(city.value).then((data) => {
+    if (map) {
+      flyTo(data.lat, data.lng);
+    } else {
+      init(data.lat, data.lng);
+    }
+  });
+};
 
 searchButton.addEventListener('click', () => {
   weatherAPI(), getSearchMap();
